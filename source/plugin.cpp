dcacf189e67a739d5a11e3bfdac4397926193bf6
Michael Fabian Dirks <info@xaymar.com>
2017-06-28 23:21:42 +0200

    Initial code
    * Shape Filter
    * Displacement Filter + Example Data

---
+++ source/plugin.cpp
@@ -0,0 +1,61 @@
+/*
+ * Modern effects for a modern Streamer
+ * Copyright (C) 2017 Michael Fabian Dirks
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+*/
+
+#include "plugin.h"
+#include "filter-displacement.h"
+#include "filter-shape.h"
+
+OBS_DECLARE_MODULE();
+OBS_MODULE_AUTHOR("Michael Fabian Dirks");
+OBS_MODULE_USE_DEFAULT_LOCALE("obs-stream-effects", "en-US");
+
+Filter::Displacement *filterDisplacement;
+Filter::Shape *filterShape;
+
+MODULE_EXPORT bool obs_module_load(void) {
+	filterDisplacement = new Filter::Displacement();
+	filterShape = new Filter::Shape();
+
+	return true;
+}
+
+MODULE_EXPORT void obs_module_unload(void) {
+	delete filterShape;
+	delete filterDisplacement;
+}
+
+MODULE_EXPORT const char* obs_module_name() {
+	return PLUGIN_NAME;
+}
+
+MODULE_EXPORT const char* obs_module_description() {
+	return PLUGIN_NAME;
+}
+
+#ifdef _WIN32
+#define NOMINMAX
+#define NOINOUT
+
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE, DWORD, LPVOID) {
+	return TRUE;
+}
+#endif
+
