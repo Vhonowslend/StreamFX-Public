f78be9c387b605b17ed30bcf79646923575c0783
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2018-09-28 21:21:04 +0200

    gs-texture: Undo removal of mip_data check

--- source/gs-texture.cpp
+++ source/gs-texture.cpp
@@ -40,8 +40,8 @@ gs::texture::texture(uint32_t width, uint32_t height, gs_color_format format, ui
 		throw std::logic_error("height must be at least 1");
 	if (mip_levels == 0)
 		throw std::logic_error("mip_levels must be at least 1");
-	//if (!mip_data)
-	//	throw std::logic_error("mip_data is invalid");
+	if (!mip_data)
+		throw std::logic_error("mip_data is invalid");
 
 	if (mip_levels > 1 || ((texture_flags & flags::BuildMipMaps) == flags::BuildMipMaps)) {
 		bool isPOT = util::math::is_power_of_two(width) && util::math::is_power_of_two(height);
