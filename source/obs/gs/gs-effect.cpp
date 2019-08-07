b88cc0fe016faf861470ce7b967d989743941278
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2019-08-07 17:07:54 +0200

    gs-effect: Check correct size in get_default_string

--- source/obs/gs/gs-effect.cpp
+++ source/obs/gs/gs-effect.cpp
@@ -737,7 +737,7 @@ void gs::effect_parameter::get_default_string(std::string& v)
 {
 	if (get_type() != type::String)
 		throw std::bad_cast();
-	size_t   ptr_len = gs_effect_get_val_size(_param);
+	size_t   ptr_len = gs_effect_get_default_val_size(_param);
 	uint8_t* ptr     = static_cast<uint8_t*>(gs_effect_get_default_val(_param));
 	if (ptr) {
 		v = std::string(ptr, ptr + ptr_len);
