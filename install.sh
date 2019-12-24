a3b1fc0b2ef8078b05cde21d71bfac18f0959f8f
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2019-12-24 08:22:06 +0100

    project: Remove git hooks
    These just didn't work as expected, and now that clang-format is integrated into the CMake project they are also no longer needed.

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-readarray -t hooks < <(find .hooks/ -maxdepth 1 -type d -not -wholename .hooks/ -print0)
-for v in "${hooks[@]}"; do
-	hookname=`basename $v`
-	echo "#!/bin/bash" > .git/hooks/${hookname}
-	echo "find .hooks/${hookname}/ -type f -name '*.sh' -exec '{}' \;" >> .git/hooks/${hookname}
-done
