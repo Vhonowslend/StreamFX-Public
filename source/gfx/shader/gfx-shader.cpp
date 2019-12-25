907216f143c3869f4fda9d80801d02325fd7d664
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2019-12-25 10:10:06 +0100

    gfx-shader: Don't show parameters that are invisible

--- source/gfx/shader/gfx-shader.cpp
+++ source/gfx/shader/gfx-shader.cpp
@@ -243,7 +243,8 @@ bool gfx::shader::shader::on_properties_modified(obs_properties_t* props, obs_pr
 
 		// Rebuild new parameters.
 		for (auto kv : _shader_params) {
-			kv.second->properties(grp, data);
+			if (kv.second->is_visible())
+				kv.second->properties(grp, data);
 			kv.second->defaults(data);
 			kv.second->update(data);
 		}
