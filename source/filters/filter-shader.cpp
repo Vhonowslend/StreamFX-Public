bb2f43a62922b03adbb19f5d825594fe0c79470f
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2020-03-31 22:22:55 +0200

    filter-shader: Fix incorrect shader mode

--- source/filters/filter-shader.cpp
+++ source/filters/filter-shader.cpp
@@ -28,7 +28,7 @@
 filter::shader::shader_instance::shader_instance(obs_data_t* data, obs_source_t* self)
 	: obs::source_instance(data, self)
 {
-	_fx = std::make_shared<gfx::shader::shader>(self, gfx::shader::shader_mode::Transition);
+	_fx = std::make_shared<gfx::shader::shader>(self, gfx::shader::shader_mode::Filter);
 	_rt = std::make_shared<gs::rendertarget>(GS_RGBA, GS_ZS_NONE);
 	update(data);
 }
