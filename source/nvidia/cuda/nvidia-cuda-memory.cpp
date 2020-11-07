edd8eb8bdaaad69ddbdceb019099eccef672492f
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2020-11-07 09:00:12 +0100

    nvidia/cuda: Log extra information when built for Debug

--- source/nvidia/cuda/nvidia-cuda-memory.cpp
+++ source/nvidia/cuda/nvidia-cuda-memory.cpp
@@ -19,9 +19,26 @@
 
 #include "nvidia-cuda-memory.hpp"
 #include <stdexcept>
+#include "util/util-logging.hpp"
+
+#ifdef _DEBUG
+#define ST_PREFIX "<%s> "
+#define D_LOG_ERROR(x, ...) P_LOG_ERROR(ST_PREFIX##x, __FUNCTION_SIG__, __VA_ARGS__)
+#define D_LOG_WARNING(x, ...) P_LOG_WARN(ST_PREFIX##x, __FUNCTION_SIG__, __VA_ARGS__)
+#define D_LOG_INFO(x, ...) P_LOG_INFO(ST_PREFIX##x, __FUNCTION_SIG__, __VA_ARGS__)
+#define D_LOG_DEBUG(x, ...) P_LOG_DEBUG(ST_PREFIX##x, __FUNCTION_SIG__, __VA_ARGS__)
+#else
+#define ST_PREFIX "<nvidia::cuda::memory> "
+#define D_LOG_ERROR(...) P_LOG_ERROR(ST_PREFIX __VA_ARGS__)
+#define D_LOG_WARNING(...) P_LOG_WARN(ST_PREFIX __VA_ARGS__)
+#define D_LOG_INFO(...) P_LOG_INFO(ST_PREFIX __VA_ARGS__)
+#define D_LOG_DEBUG(...) P_LOG_DEBUG(ST_PREFIX __VA_ARGS__)
+#endif
 
 nvidia::cuda::memory::memory(size_t size) : _cuda(::nvidia::cuda::cuda::get()), _pointer(), _size(size)
 {
+	D_LOG_DEBUG("Initializating... (Addr: 0x%" PRIuPTR ")", this);
+
 	::nvidia::cuda::result res = _cuda->cuMemAlloc(&_pointer, _size);
 	switch (res) {
 	case ::nvidia::cuda::result::SUCCESS:
@@ -33,6 +50,8 @@ nvidia::cuda::memory::memory(size_t size) : _cuda(::nvidia::cuda::cuda::get()), 
 
 nvidia::cuda::memory::~memory()
 {
+	D_LOG_DEBUG("Finalizing... (Addr: 0x%" PRIuPTR ")", this);
+
 	_cuda->cuMemFree(_pointer);
 }
 nvidia::cuda::device_ptr_t nvidia::cuda::memory::get()
