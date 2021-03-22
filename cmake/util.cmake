d8bb1cecc535ce662fa4e444e484ad3af91de47e
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2021-03-22 22:14:19 +0100

    cmake: (MacOS) Fix linker path for libraries

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -1,3 +1,5 @@
+include(CMakeParseArguments)
+
 Function(CacheSet Name Value)
 	GET_PROPERTY(V_ADVANCED CACHE "${Name}" PROPERTY ADVANCED)
 	GET_PROPERTY(V_TYPE CACHE "${Name}" PROPERTY TYPE)
@@ -15,5 +17,38 @@ Function(CacheClear Name)
 	Set(${Name} 0 CACHE ${V_TYPE} ${V_HELPSTRING} FORCE)
 	If(${V_ADVANCED})
 		Mark_As_Advanced(FORCE ${Name})
-	EndIf()	
+	EndIf()
 EndFunction()
+
+function(mac_get_linker_id)
+	cmake_parse_arguments(
+		_MGLI "" "TARGET;OUTPUT" "" ${ARGN}
+	)
+
+	get_target_property(_MGLI_TARGET_LOC ${_MGLI_TARGET} IMPORTED_LOCATION)
+	if(NOT _MGLI_TARGET_LOC)
+		get_target_property(_MGLI_TARGET_LOC ${_MGLI_TARGET} IMPORTED_LOCATION_MINSIZEREL)
+	endif()
+	if(NOT _MGLI_TARGET_LOC)
+		get_target_property(_MGLI_TARGET_LOC ${_MGLI_TARGET} IMPORTED_LOCATION_RELEASE)
+	endif()
+	if(NOT _MGLI_TARGET_LOC)
+		get_target_property(_MGLI_TARGET_LOC ${_MGLI_TARGET} IMPORTED_LOCATION_RELWITHDEBINFO)
+	endif()
+	if(NOT _MGLI_TARGET_LOC)
+		get_target_property(_MGLI_TARGET_LOC ${_MGLI_TARGET} IMPORTED_LOCATION_DEBUG)
+	endif()
+
+	execute_process(
+		COMMAND otool -D ${_MGLI_TARGET_LOC}
+		WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+		RESULT_VARIABLE _MGLI_RES
+		OUTPUT_VARIABLE _MGLI_LINK OUTPUT_STRIP_TRAILING_WHITESPACE
+		ERROR_VARIABLE _MGLI_ERR ERROR_STRIP_TRAILING_WHITESPACE ERROR_QUIET
+	)
+	STRING(REGEX REPLACE ";" "\\\\;" _MGLI_LINK "${_MGLI_LINK}")
+	STRING(REGEX REPLACE "\n" ";" _MGLI_LINK "${_MGLI_LINK}")
+	list(POP_FRONT _MGLI_LINK)
+
+	set(${_MGLI_OUTPUT} "${_MGLI_LINK}" PARENT_SCOPE)
+endfunction()
