901ffb8ae2b86cfca68b6cea35ffc03f41eae874
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2021-04-14 12:01:47 +0200

    source/shader: Fix missing buffered draw requirement (#516)
    Fixes #516

--- source/sources/source-shader.cpp
+++ source/sources/source-shader.cpp
@@ -101,7 +101,7 @@ shader_factory::shader_factory()
 {
 	_info.id           = PREFIX "source-shader";
 	_info.type         = OBS_SOURCE_TYPE_INPUT;
-	_info.output_flags = OBS_SOURCE_VIDEO;
+	_info.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_CUSTOM_DRAW;
 
 	set_activity_tracking_enabled(true);
 	finish_setup();
