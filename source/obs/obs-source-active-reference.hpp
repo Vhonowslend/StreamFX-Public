02ddf66a2bc529ec00c735be78ded8562ad1075d
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2022-05-14 06:29:41 +0200

    obs/source: Refactor old deprecated classes

--- source/obs/obs-source-active-reference.hpp
+++ source/obs/obs-source-active-reference.hpp
@@ -1,6 +1,5 @@
 /*
- * Modern effects for a modern Streamer
- * Copyright (C) 2018 Michael Fabian Dirks
+ * Copyright (C) 2022 Michael Fabian Dirks
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,4 +16,33 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+#pragma once
+#include "common.hpp"
 #include "obs-source.hpp"
+#include "obs-weak-source.hpp"
+
+namespace streamfx::obs {
+	class source_active_reference {
+		::streamfx::obs::weak_source _target;
+
+		public:
+		~source_active_reference()
+		{
+			auto v = _target.lock();
+			if (v) {
+				v.decrement_active();
+			}
+		}
+		source_active_reference(::streamfx::obs::source& source) : _target(source)
+		{
+			source.increment_active();
+		}
+
+		public:
+		static FORCE_INLINE std::shared_ptr<source_active_reference>
+							add_active_reference(::streamfx::obs::source& source)
+		{
+			return std::make_shared<source_active_reference>(source);
+		}
+	};
+} // namespace streamfx::obs
--- source/obs/obs-source-showing-reference.hpp
+++ source/obs/obs-source-showing-reference.hpp
@@ -1,6 +1,5 @@
 /*
- * Modern effects for a modern Streamer
- * Copyright (C) 2018 Michael Fabian Dirks
+ * Copyright (C) 2022 Michael Fabian Dirks
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,4 +16,34 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+#pragma once
+#include "common.hpp"
 #include "obs-source.hpp"
+#include "obs-tools.hpp"
+#include "obs-weak-source.hpp"
+
+namespace streamfx::obs {
+	class source_showing_reference {
+		::streamfx::obs::weak_source _target;
+
+		public:
+		~source_showing_reference()
+		{
+			auto v = _target.lock();
+			if (v) {
+				v.decrement_showing();
+			}
+		}
+		source_showing_reference(::streamfx::obs::source& source) : _target(source)
+		{
+			source.increment_showing();
+		}
+
+		public:
+		static FORCE_INLINE std::shared_ptr<source_showing_reference>
+							add_showing_reference(::streamfx::obs::source& source)
+		{
+			return std::make_shared<source_showing_reference>(source);
+		}
+	};
+} // namespace streamfx::obs
